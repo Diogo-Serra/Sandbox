cae11d24f289943cb9ac57d078210dfe7af183d8
Di Code <di-code@outlook.com>
2025-09-17 00:09:34 +0100

    updates

---
+++ memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memchr.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/16 23:53:08 by diserra           #+#    #+#             */
+/*   Updated: 2025/09/17 00:09:08 by diserra          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void *memchr(const void *s, int c, size_t n)
+{
+	const unsigned char *p;
+
+	p = (const unsigned char *)s;
+	while (n--)
+	{
+		if (*p == (unsigned char)c)
+			return (void *)p;
+		p++;
+	}
+	return (NULL);
+}
