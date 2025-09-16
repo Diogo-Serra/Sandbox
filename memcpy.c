31ca8f21cd957936005b69adf4df49bee8960270
Di Code <di-code@outlook.com>
2025-09-16 21:53:39 +0100

    updates

---
+++ memcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memcpy.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/15 18:37:03 by diserra           #+#    #+#             */
+/*   Updated: 2025/09/15 18:42:57 by diserra          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*d;
+	const unsigned char	*s;
+
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	while (n--)
+		*d++ = *s++;
+	return (dest);
+}
