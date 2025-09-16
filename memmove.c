47937af640ca383c7be3e2c13471e3d5e5c5077a
Di Code <di-code@outlook.com>
2025-09-16 23:44:33 +0100

    updates

--- memmove.c
+++ memmove.c
@@ -6,17 +6,28 @@
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 23:12:53 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/16 23:24:19 by diserra          ###   ########.fr       */
+/*   Updated: 2025/09/16 23:40:24 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memmove(void *dest, const void *src, size_t n)
+void *memmove(void *dest, const void *src, size_t n)
 {
 	unsigned const char *s;
 	unsigned char *d;
 
 	s = (const unsigned char *)src;
 	d = (unsigned char *)dest;
+	if (d > s)
+	{
+		while (n--)
+			d[n] = s[n];
+	}
+	else
+	{
+		while (n--)
+			*d++ = *s++;
+	}
+	return (dest);
 }
