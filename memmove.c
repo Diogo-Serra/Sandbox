78661a9a3b4095959c9f6e139b3d06826002b722
Di Code <di-code@outlook.com>
2025-09-16 23:13:48 +0100

    updates

--- memmove.c
+++ memmove.c
@@ -1,25 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   memcpy.c                                           :+:      :+:    :+:   */
+/*   memmove.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/16 22:13:32 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/16 23:11:06 by diserra          ###   ########.fr       */
+/*   Created: 2025/09/16 23:12:53 by diserra           #+#    #+#             */
+/*   Updated: 2025/09/16 23:13:04 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memcpy(void *dest, const void *src, size_t n)
-{
-	const unsigned char *s;
-	unsigned char *d;
-		
-	s = (const unsigned char *)src;
-	d = (unsigned char *)dest;
-	while (n--)
-		*d++ = *s++;
-	return (dest);
-}
