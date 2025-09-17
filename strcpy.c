e75c6ae9ba5ecccc3fd96ca2ee47891b3e3d726e
Di Code <di-code@outlook.com>
2025-09-17 01:15:29 +0100

    updates

--- strcpy.c
+++ strcpy.c
@@ -1,32 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strdup.c                                           :+:      :+:    :+:   */
+/*   strcpy.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: diserra <diserra@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/17 00:44:50 by diserra           #+#    #+#             */
-/*   Updated: 2025/09/17 01:13:04 by diserra          ###   ########.fr       */
+/*   Created: 2025/09/17 01:12:47 by diserra           #+#    #+#             */
+/*   Updated: 2025/09/17 01:15:24 by diserra          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-char	*strdup(const char *s)
+char	*strcpy(char *dest, const char *src)
 {
-	char	*out;
-	size_t	len;
-	size_t	i;
-
-	len = strlen(s);
-	out = malloc(sizeof(char) * (len + 1));
-	i = 0;
-	if (!out)
-		return (NULL);
-	while (i <= len)
-	{
-		out[i] = s[i];
-		i++;
-	}
-	return (out);
+	
 }
