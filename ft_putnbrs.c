7e61ef0d281138e289d8ab920bd9ac1a90aa5aee
Diogo Serra <di-code@outlook.com>
2025-10-25 14:29:13 +0100

    Remove unused ft_memset function
    Removed unused ft_memset function from ft_putnbrs.c.

--- ft_putnbrs.c
+++ ft_putnbrs.c
@@ -12,16 +12,6 @@
 
 #include "libft.h"
 
-void	*ft_memset(void *s, int c, size_t n)
-{
-	unsigned char	*p;
-
-	p = (unsigned char *)s;
-	while (n--)
-		*p++ = (unsigned char)c;
-	return (s);
-}
-
 void	ft_putnbrs(int c)
 {
 	char	nbr[21];
