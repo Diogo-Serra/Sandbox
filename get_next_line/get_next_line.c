56bd383df5aef2d7f8328529fa2a61f8d3083dda
Diogo-Serra <di-code@outlook.com>
2025-11-27 15:07:43 +0000

    updates

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 19:26:08 by diosoare          #+#    #+#             */
-/*   Updated: 2025/11/27 15:00:25 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/11/27 15:07:30 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,13 @@
 
 int	main(void)
 {
-	printf("Test1 %zu\n", ft_strlen("Hello"));
+	char *out;
+	char *source1;
+	
+	source1 = ft_strdup("Hello");
+	out = ft_strjoin(source1, " World!");
+	printf("Test1 %s\n", out);
+	free(out);
 	return (0);
 }
 /* 
