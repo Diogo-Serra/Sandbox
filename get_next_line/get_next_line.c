ff78085c8ea04d2418190e8cc4e001f394161b58
Diogo-Serra <di-code@outlook.com>
2025-11-27 14:59:39 +0000

    updates

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 19:26:08 by diosoare          #+#    #+#             */
-/*   Updated: 2025/11/27 14:58:02 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/11/27 14:59:37 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,10 @@
 
 int	main(void)
 {
-	get_next_line(open("test.txt", O_RDONLY));
+	
 	return (0);
 }
-
+/* 
 char	*get_next_line(int fd)
-{
-	printf("%zu", ft_strlen("Hello"));
-	return (0);		
-}
+{	
+} */
