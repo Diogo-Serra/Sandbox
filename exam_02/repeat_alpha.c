906f9ee21c984d18b38dc12c48eceb0ce5742000
Diogo-Serra <di-code@outlook.com>
2025-12-04 22:24:19 +0000

    updates

--- exam_02/repeat_alpha.c
+++ exam_02/repeat_alpha.c
@@ -6,9 +6,26 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 22:12:59 by diosoare          #+#    #+#             */
-/*   Updated: 2025/12/04 22:13:13 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/12/04 22:22:43 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	repeat_alpha(void)
+{
+	char	alpha;
+	int		i;
+	int		d;
+
+	alpha = 'a';
+	i = 1;
+	while (alpha <= 'z')
+	{
+		d = i;
+		while (d--)
+			write(1, &alpha, 1);
+		i++;
+		alpha++;
+	}
+}
