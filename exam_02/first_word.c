a0f15856f338dfe76f09b68b96da8d5806578ce9
Diogo-Serra <di-code@outlook.com>
2025-12-04 20:27:05 +0000

    updates

--- exam_02/first_word.c
+++ exam_02/first_word.c
@@ -6,7 +6,26 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 20:04:42 by diosoare          #+#    #+#             */
-/*   Updated: 2025/12/04 20:04:43 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/12/04 20:26:53 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	first_word(char	*src)
+{
+	while (*src)
+	{
+		while (*src == ' ')
+			src++;
+		while (*src != ' ')
+			write(1 , &src, 1);		
+	}
+	write(1 , '\n', 1);		
+}
+
+int	main(void)
+{
+	first_word("Escola 42 Teste");
+	return (0);
+}
