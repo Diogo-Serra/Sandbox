6f8e260f9565a0a2797dd8f264a5dab7622f48aa
Diogo-Serra <di-code@outlook.com>
2025-12-04 21:21:59 +0000

    updates

--- exam_02/ft_strcpy.c
+++ exam_02/ft_strcpy.c
@@ -6,23 +6,25 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 21:15:01 by diosoare          #+#    #+#             */
-/*   Updated: 2025/12/04 21:20:11 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/12/04 21:21:53 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-char	*ft_strcpy(char *dest,const char *src)
+char	*ft_strcpy(char *dest, const char *src)
 {
+	char	*out = dest;
+
 	while (*src)
 		*dest++ = *src++;
 	*dest = '\0';
-	return (dest);
+	return (out);
 }
 
 int	main()
 {
-	char	dest[5];
+	char	dest[6];
 	
 	ft_strcpy(dest, "Hello");
 	printf("Teste: %s\n", dest);	
