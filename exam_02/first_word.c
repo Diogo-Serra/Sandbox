3d93e25b8f298d4653430d79e02190a9b6c1fd75
Diogo-Serra <di-code@outlook.com>
2025-12-04 20:32:21 +0000

    updates

--- exam_02/first_word.c
+++ exam_02/first_word.c
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 20:04:42 by diosoare          #+#    #+#             */
-/*   Updated: 2025/12/04 20:26:53 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/12/04 20:32:08 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 
 void	first_word(char	*src)
 {
-	while (*src)
+
+	while (*src == ' ')
+		src++;
+	while (*src && *src != ' ')
 	{
-		while (*src == ' ')
-			src++;
-		while (*src != ' ')
-			write(1 , &src, 1);		
-	}
-	write(1 , '\n', 1);		
+		write(1 , src, 1);
+		src++;
+	}		
+	write(1 , "\n", 1);
 }
 
 int	main(void)
