e24d5bc2cc513614fc274ee84d4f05df8b049b12
Diogo-Serra <di-code@outlook.com>
2025-12-04 20:38:48 +0000

    updates

--- exam_02/first_word.c
+++ exam_02/first_word.c
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 20:04:42 by diosoare          #+#    #+#             */
-/*   Updated: 2025/12/04 20:38:38 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/12/04 20:38:46 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,3 @@ void	first_word(char	*src)
 		write(1 , src++, 1);
 	write(1 , "\n", 1);
 }
-
-/* int	main(void)
-{
-	char *str = NULL;
-	first_word(str);
-	return (0);
-} */
