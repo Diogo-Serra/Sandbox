9502ca4de72959fbc6325827d4080ad5c187d7b6
Diogo-Serra <di-code@outlook.com>
2025-12-04 22:13:32 +0000

    updates

--- exam_02/ft_strcpy.c
+++ exam_02/ft_strcpy.c
@@ -6,11 +6,9 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/04 21:15:01 by diosoare          #+#    #+#             */
-/*   Updated: 2025/12/04 21:48:45 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/12/04 22:13:26 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 char	*ft_strcpy(char *dest, const char *src)
 {
