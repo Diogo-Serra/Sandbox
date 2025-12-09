3b5f5ac02d9addff0a9eb337d5e79f666255c4ca
Diogo-Serra <di-code@outlook.com>
2025-12-09 17:00:49 +0000

    updates

---
+++ exam_02/lvl2/ft_atoi.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 16:53:19 by diosoare          #+#    #+#             */
+/*   Updated: 2025/12/09 17:00:27 by diosoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int	result;
+	int	sign;
+
+	while ((*str == ' ') || (*str >= 9 && *str <= 13))
+		str++;
+	sign = 1;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (*str)
+		result = (result * 10) + (*str++ - '0');
+	return (result * sign);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		printf("%d\n", ft_atoi(argv[1]));
+	else
+		printf("\n");
+	return (0);
+}
