641d7ed119908fb30366d739485e4ec8f8a6acaf
Diogo-Serra <di-code@outlook.com>
2025-12-09 14:48:58 +0000

    updates

--- exam_02/lvl2/alpha_mirror.c
+++ exam_02/lvl2/alpha_mirror.c
@@ -6,11 +6,9 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 14:23:02 by diosoare          #+#    #+#             */
-/*   Updated: 2025/12/09 14:46:47 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/12/09 14:48:56 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 void	alpha_mirror(char *str)
 {
