972800de417a934ac014b9cc40ac8aee41b32a66
Diogo-Serra <di-code@outlook.com>
2025-12-09 14:46:49 +0000

    updates

--- exam_02/lvl2/alpha_mirror.c
+++ exam_02/lvl2/alpha_mirror.c
@@ -6,29 +6,24 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 14:23:02 by diosoare          #+#    #+#             */
-/*   Updated: 2025/12/09 14:41:30 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/12/09 14:46:47 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdio.h>
 
 void	alpha_mirror(char *str)
 {
 	while (*str)
 	{
-		if (*str >= 'A' && *str < 'N')
+		if (*str >= 'A' && *str <= 'M')
 			*str += 13;
-		if (*str >= 'a' && *str < 'n')
+		else if (*str >= 'a' && *str <= 'm')
 			*str += 13;
-		if (*str > 'M' && *str <= 'Z')
-			*str += 13 - 25;
-		if (*str > 'm' && *str <= 'z')
-			*str += 13 - 25;
+		else if (*str > 'M' && *str <= 'Z')
+			*str -= 12;
+		else if (*str > 'm' && *str <= 'z')
+			*str -= 12;
+		str++;
 	}
-}
-
-int	main(void)
-{
-	char	str[] = "Hello";
-	alpha_mirror(str);
-	printf("%s\n", str);
-	return (0);
 }
