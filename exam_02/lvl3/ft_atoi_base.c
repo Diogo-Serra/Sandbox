eed5012f9f3cb92e4fbb09e392c6017647f184f4
Diogo-Serra <di-code@outlook.com>
2025-12-10 02:10:12 +0000

    updates

---
+++ exam_02/lvl3/ft_atoi_base.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/10 01:52:45 by diosoare          #+#    #+#             */
+/*   Updated: 2025/12/10 02:07:01 by diosoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_atoi_base(char *str, int base)
+{
+	ssize_t	result;	
+	int		sign;
+
+	sign = 1;
+	result = 0;
+
+	while (*str == ' ' || *str >= 9 && *str <= 13)
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (*str)
+	{
+		digit = -1;
+		if (*str >= '0' && *str <= '9')
+			digit = *str - '0';
+		else if (*str >= 'a' && *str <= 'f')
+			digit = *str - 'a' + 10;
+		else if (*str >= 'A' && *str <= 'F')
+			digit = *str - 'A' + 10;
+		if (digit == -1 || digit >= base)
+			break;
+		result = result * base + digit;
+		str++;
+	}
+	return (result * sign);
+}
