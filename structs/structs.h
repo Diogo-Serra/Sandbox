2bda75480237d0ec397a8506d934b3cc2f4d30c9
Diogo-Serra <di-code@outlook.com>
2025-12-12 17:17:31 +0000

    updates

--- structs/structs.h
+++ structs/structs.h
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 15:01:05 by diosoare          #+#    #+#             */
-/*   Updated: 2025/12/12 17:17:16 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/12/12 17:17:31 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,5 @@ void	print_list(t_student *head);
 void	free_list(t_student **head, t_student **tail);
 void	add_front(t_student **head, t_student **tail, t_student *new_node);
 void	add_back(t_student **head, t_student **tail, t_student *new_node);
-
 
 #endif /* STRUCTS_H */
