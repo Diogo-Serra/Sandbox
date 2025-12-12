4223e095eb3fbfaddce0cd123296c0bcb009708b
Diogo-Serra <di-code@outlook.com>
2025-12-12 16:13:51 +0000

    updates

--- structs/struct1.c
+++ structs/struct1.c
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 22:30:49 by diosoare          #+#    #+#             */
-/*   Updated: 2025/12/12 15:07:39 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/12/12 15:12:38 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ void	create_coordinates(void)
 {
 	t_point	coordinate_1;
 
-	coordinate_1.x = 0;
-	coordinate_1.y = 0;
-	printf("1x: %d\n1y: %d\n2x: %d\n2y: %d\n", coordinate_1.x, coordinate_1.y);
+	coordinate_1.x = 42;
+	coordinate_1.y = 42;
+	printf("Coordinate Created:\nx: %d\ny: %d\n", coordinate_1.x, coordinate_1.y);
 }
 
 int	main(void)
@@ -27,7 +27,7 @@ int	main(void)
 
 	first_coordinate.x = 5;
 	first_coordinate.y = 10;
-	printf("x: %d\ny: %d\n", first_coordinate.x, first_coordinate.y);
+	printf("First Coordinate:\nx: %d\ny: %d\n", first_coordinate.x, first_coordinate.y);
 	create_coordinates();
 	return (0);
 }
--- structs/struct2.c
+++ structs/struct2.c
@@ -6,11 +6,32 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 17:49:33 by diosoare          #+#    #+#             */
-/*   Updated: 2025/12/12 15:08:33 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/12/12 16:13:28 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "structs.h"
+
+void	free_list(t_student **head)
+{
+	t_student	*current;
+	t_student	*temp;
+	
+	current = *head;
+	while (current)
+	{
+		temp = current->next;
+		free(current);
+		current = temp;
+	}
+	*head = NULL;
+}
+
+void	add_front(t_student **head, t_student *new_node)
+{
+	new_node->next = *head;
+	*head = new_node;
+}
 
 t_student	*create_student(char *name, int id, float grade)
 {
@@ -35,10 +56,17 @@ t_student	*create_student(char *name, int id, float grade)
 
 int	main(void)
 {
-	t_student	*student;
+	t_student	*head;
+	t_student	*s1;
+	t_student	*s2;
 
-	student = create_student("Diogo", 35, 18.5);
-	printf("%s\n%d\n%f\n", student->name, student->student_id, student->grade);
-	free (student);
+	head = NULL;
+	s1 = create_student("Diogo", 35, 18.5);
+	printf("%s\n%d\n%f\n", s1->name, s1->student_id, s1->grade);
+	add_front(&head, s1);
+	s2 = create_student("Maria", 30, 19.5);
+	add_front(&head, s2);
+	printf("%s\n%d\n%f\n", s2->name, s2->student_id, s2->grade);
+	free_list(&head);
 	return (0);
 }
