b23adfd1573dbf5015cc2d90ed5fd470545054db
Diogo-Serra <di-code@outlook.com>
2025-12-13 16:49:11 +0000

    updates

--- structs2/struct.h
+++ structs2/struct.h
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 16:36:23 by diosoare          #+#    #+#             */
-/*   Updated: 2025/12/13 16:38:28 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/12/13 16:49:09 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 # define STRUCT_H
 
 # include <stdio.h>
+
+typedef struct s_player
+{
+	int		id;
+	int		score;
+	char	name[50];	
+}	t_player;
 
 #endif /* STRUCT_H */
