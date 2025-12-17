668c0dc48e4d16fde13576fd81606c8dc6ad29cc
Diogo-Serra <di-code@outlook.com>
2025-12-17 11:34:37 +0000

    updates

--- exam_02/lvl3/epur_str.c
+++ exam_02/lvl3/epur_str.c
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 10:34:52 by diosoare          #+#    #+#             */
-/*   Updated: 2025/12/17 11:08:00 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/12/17 11:34:25 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,32 @@ char	*epur_str(char *str)
 {
 	size_t	i;
 	size_t	j;
+	size_t	len;
 	char	*out;
 
 	if (!str)
 		return (NULL);
-	i = 0;
-	while (str[i] && (str[i] == ' ' || str[i] == '\t'))
-		i++;
-	if (str[i] == '\0')
-		return (NULL);
-	out = malloc((i + 1) * sizeof(char));
+	len = 0;
+	while (str[len])
+		len++;
+	out = malloc((len + 1) * sizeof(char));
 	if (!out)
 		return (NULL);
+	i = 0;
 	j = 0;
-	while (str[i] && (str[i] != ' ' || str[i] != '\t'))
-		out[j++] = str[i++];
+	while (str[i] == ' ' || str[i] == '\t')
+		i++;
+	while (str[i])
+	{
+		if (str[i] == ' ' || str[i] == '\t')
+		{
+			if (str[i + 1] && str[i + 1] != ' ' && str[i + 1] != '\t')
+				out[j++] = ' ';
+		}
+		else
+			out[j++] = str[i];
+		i++;
+	}
 	out[j] = '\0';
 	return (out);
 }
