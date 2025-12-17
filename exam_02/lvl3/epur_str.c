6d267e0899f0ab8a9548e3658d4ddddaf5c5f6ec
Diogo-Serra <di-code@outlook.com>
2025-12-17 11:07:04 +0000

    updates

--- exam_02/lvl3/epur_str.c
+++ exam_02/lvl3/epur_str.c
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 10:34:52 by diosoare          #+#    #+#             */
-/*   Updated: 2025/12/17 10:48:19 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/12/17 11:06:55 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,33 +16,31 @@
 char	*epur_str(char *str)
 {
 	size_t	i;
+	size_t	j;
 	char	*out;
 
-	out = NULL;
 	if (!str)
 		return (NULL);
 	i = 0;
-	while (str[i] || str[i] == ' ' || str[i] == '\t')
+	while (str[i] && str[i] == ' ' || str[i] == '\t')
 		i++;
-	while (str[i] || str[i] != ' ' || str[i] != '\t')
-		i++;
+	if (str[i] == '\0')
+		return (NULL);
 	out = malloc((i + 1) * sizeof(char));
 	if (!out)
 		return (NULL);
-	out[i-- + 1] = '\0';
-	i = 0;
-	while (*str || *str == ' ' || *str == '\t')
-		str++;
-	while (*str || *str != ' ' || *str != '\t')
-		out[i++] = *str++;
-	return (out);
+	j = 0;
+	while (str[i] && str[i] != ' ' || str[i] != '\t')
+		out[j++] = str[i++];
+	out[j] = '\0';
+	return (out);	
 }
 
 int	main(void)
 {
 	char	*out;
 
-	out = epur_str("   Hello World   ");
+	out = epur_str("  Hello  ");
 	printf("%s\n", out);
 	return (0);
 }
