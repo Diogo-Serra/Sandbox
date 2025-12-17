10dac40e930454dc6b30818934942db0472c91ee
Diogo-Serra <di-code@outlook.com>
2025-12-17 09:16:18 +0000

    updates

--- struct/struct.h
+++ struct/struct.h
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 09:06:15 by diosoare          #+#    #+#             */
-/*   Updated: 2025/12/17 09:11:40 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/12/17 09:15:26 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,9 @@ typedef struct s_person
 	struct s_person	*next;
 }	t_person;
 
-void	add_front(t_person **head, t_person *new_node);
-void	add_back(t_person **head, t_person *new_node);
+void		add_front(t_person **head, t_person *new_node);
+void		add_back(t_person **head, t_person *new_node);
+void		print_list(t_person **head);
+t_person	*add_person(int age, char *name);
 
 #endif
