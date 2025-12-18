f8eea142abf4018e940030ea7932290bf48d775b
Diogo-Serra <di-code@outlook.com>
2025-12-18 19:06:04 +0000

    updates

--- exam_02/lvl3/ft_rrange.c
+++ exam_02/lvl3/ft_rrange.c
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 18:57:48 by diosoare          #+#    #+#             */
-/*   Updated: 2025/12/18 19:00:19 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/12/18 19:05:55 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,33 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int	*ft_range(int start, int end)
+int	*ft_rrange(int start, int end)
 {
 	int		*arr;
 	int		count;
 	int		i;
 
-	count = end - start + 1;
+	if (start <= end)
+		count = end - start + 1;
+	else
+		count = start - end + 1;
 	arr = malloc(count * sizeof(int));
 	if (!arr)
 		return (0);
 	i = 0;
 	while (i < count)
-		arr[count--] = end--;
+	{
+		arr[i] = end;
+		if (start <= end)
+			end--;
+		else
+			end++;
+		i++;
+	}
 	return (arr);
 }
 
-/* int	main(void)
+int	main(void)
 {
 	int	*arr;
 	int	count;
@@ -41,9 +51,9 @@ int	*ft_range(int start, int end)
 	start = 1;
 	end = 10;
 	count = end - start + 1;
-	arr = ft_range(start, end);
+	arr = ft_rrange(start, end);
 	i = 0;
 	while (i < count)
 		printf("%d\n", arr[i++]);
 	return (0);
-} */
+}
