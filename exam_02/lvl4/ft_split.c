ed87e4c0e2d5ca427eeabdbe90d3bbea1289166e
Diogo Alexandre Soares Serra <di-code@outlook.com>
2025-12-19 13:28:17 +0000

    updates

--- exam_02/lvl4/ft_split.c
+++ exam_02/lvl4/ft_split.c
@@ -6,13 +6,23 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/19 13:09:33 by diosoare          #+#    #+#             */
-/*   Updated: 2025/12/19 13:25:21 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/12/19 13:27:30 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+
+size_t	word_len(char *src, char sep)
+{
+	size_t	len;
+
+	len = 0;
+	while (src[len] || src[len] == sep)
+		len++;
+	return (len);
+}
 
 size_t	count_words(char *src, char sep)
 {
