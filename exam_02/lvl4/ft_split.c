b09e39da455c07dce37b7a2045244230c645d2d6
Diogo Alexandre Soares Serra <di-code@outlook.com>
2025-12-19 14:50:46 +0000

    updates

--- exam_02/lvl4/ft_split.c
+++ exam_02/lvl4/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/19 13:09:33 by diosoare          #+#    #+#             */
-/*   Updated: 2025/12/19 13:30:11 by diosoare         ###   ########.fr       */
+/*   Updated: 2025/12/19 14:50:26 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,17 +40,79 @@ size_t	count_words(char *src, char sep)
 	return (count);
 }
 
-/* char	**ft_split(char	*src, char	sep)
+void	free_heap(char **src, size_t n)
 {
-	
+	size_t	i;
+
+	i = 0;
+	while (n--)
+		free(src[i++]);
+	free(src);
 }
- */
+
+char	*ft_strdup(char *src, char sep)
+{
+	char	*dup;
+	size_t	wordlen;
+
+	wordlen = word_len(src, sep);
+	dup = malloc(wordlen * sizeof(char) + 1);
+	if (!dup)
+		return (NULL);
+	wordlen = 0;
+	while (src[wordlen] && src[wordlen] != sep)
+	{
+		dup[wordlen] = src[wordlen];
+		wordlen++;
+	}	
+	dup[wordlen] = '\0';
+	return (dup);
+}
+
+char	**ft_split(char	*src, char	sep)
+{
+	char	**split;
+	size_t	wordslen;
+	size_t	nwords;
+	size_t	i;
+	size_t	j;
+
+	if (!src || !sep)
+		return (NULL);
+	nwords = count_words(src, sep);
+	split = (char **)malloc(nwords * sizeof(char *));
+	if (!split)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (nwords)
+	{
+		while (src[j] == sep)
+			j++;
+		wordslen = word_len(&src[j], sep);
+		split[i] = ft_strdup(&src[j], sep);
+		if (!split[i])
+			return (free_heap(split, i), NULL);
+		j += wordslen;
+		nwords--;
+		i++;
+	}
+	return (split);
+}
 
 int	main(void)
 {
 	char	src[] = "Hello World Test!";
 	char	sep = ' ';
-
-	printf("%zu\n%zu\n", count_words(src, sep), word_len(src, sep));
+	char	**split;
+	size_t	i;
+	size_t	nwords;
+	
+	split = ft_split(src, sep);	
+	nwords = count_words(&src[0], sep);
+	i = 0;
+	while (nwords--)
+		printf("%s\n", split[i++]);
+	free_heap(split, i);
 	return (0);
 }
