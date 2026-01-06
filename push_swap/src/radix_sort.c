aae4d95c50e4b774c35181f59fda15e1bbcd9b93
Diogo-Serra <di-code@outlook.com>
2026-01-06 11:18:53 +0000

    updates

--- push_swap/src/radix_sort.c
+++ push_swap/src/radix_sort.c
@@ -6,44 +6,36 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 11:10:29 by diosoare          #+#    #+#             */
-/*   Updated: 2026/01/06 11:10:32 by diosoare         ###   ########.fr       */
+/*   Updated: 2026/01/06 11:18:43 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int	get_max_bits(t_stack *stack)
+static int	get_max_bits(int max_index)
 {
-	int	max;
 	int	bits;
 
-	max = stack_max(stack)->value;
 	bits = 0;
-	while (max >> bits)
+	while (max_index >> bits)
 		bits++;
 	return (bits);
 }
 
-static void	index_stack(t_stack **a)
+static int	get_index(t_stack *stack, int value)
 {
-	t_stack	*curr;
 	t_stack	*tmp;
 	int		index;
 
-	curr = *a;
-	while (curr)
+	tmp = stack;
+	index = 0;
+	while (tmp)
 	{
-		tmp = *a;
-		index = 0;
-		while (tmp)
-		{
-			if (curr->value > tmp->value)
-				index++;
-			tmp = tmp->next;
-		}
-		curr->value = index;
-		curr = curr->next;
+		if (value > tmp->value)
+			index++;
+		tmp = tmp->next;
 	}
+	return (index);
 }
 
 void	radix_sort(t_stack **a, t_stack **b)
@@ -52,17 +44,18 @@ void	radix_sort(t_stack **a, t_stack **b)
 	int	max_bits;
 	int	i;
 	int	j;
+	int	index;
 
 	size = stack_size(*a);
-	index_stack(a);
-	max_bits = get_max_bits(*a);
+	max_bits = get_max_bits(size - 1);
 	i = 0;
 	while (i < max_bits)
 	{
 		j = 0;
 		while (j < size)
 		{
-			if ((((*a)->value >> i) & 1) == 0)
+			index = get_index(*a, (*a)->value);
+			if (((index >> i) & 1) == 0)
 				pb(a, b);
 			else
 				ra(a);
