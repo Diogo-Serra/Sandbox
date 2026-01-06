69450082c8a94b1744f98418244f0c2349b22916
Diogo-Serra <di-code@outlook.com>
2026-01-06 11:16:54 +0000

    updates

---
+++ push_swap/src/sort_small.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_small.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/06 11:15:24 by diosoare          #+#    #+#             */
+/*   Updated: 2026/01/06 11:16:35 by diosoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static int	get_position(t_stack *stack, int value)
+{
+	int	pos;
+
+	pos = 0;
+	while (stack)
+	{
+		if (stack->value == value)
+			return (pos);
+		pos++;
+		stack = stack->next;
+	}
+	return (pos);
+}
+
+static void	sort_three(t_stack **a)
+{
+	int	first;
+	int	second;
+	int	third;
+
+	first = (*a)->value;
+	second = (*a)->next->value;
+	third = (*a)->next->next->value;
+	if (first > second && second < third && first < third)
+		sa(a);
+	else if (first > second && second > third)
+	{
+		sa(a);
+		rra(a);
+	}
+	else if (first > second && second < third && first > third)
+		ra(a);
+	else if (first < second && second > third && first < third)
+	{
+		sa(a);
+		ra(a);
+	}
+	else if (first < second && second > third && first > third)
+		rra(a);
+}
+
+static void	push_min_to_b(t_stack **a, t_stack **b)
+{
+	t_stack	*min_node;
+	int		min_pos;
+	int		size;
+
+	min_node = stack_min(*a);
+	min_pos = get_position(*a, min_node->value);
+	size = stack_size(*a);
+	if (min_pos <= size / 2)
+	{
+		while ((*a)->value != min_node->value)
+			ra(a);
+	}
+	else
+	{
+		while ((*a)->value != min_node->value)
+			rra(a);
+	}
+	pb(a, b);
+}
+
+void	sort_small(t_stack **a, t_stack **b)
+{
+	int	size;
+
+	size = stack_size(*a);
+	if (size == 2)
+	{
+		if ((*a)->value > (*a)->next->value)
+			sa(a);
+	}
+	else if (size == 3)
+		sort_three(a);
+	else if (size == 4)
+	{
+		push_min_to_b(a, b);
+		sort_three(a);
+		pa(a, b);
+	}
+	else if (size == 5)
+	{
+		push_min_to_b(a, b);
+		push_min_to_b(a, b);
+		sort_three(a);
+		pa(a, b);
+		pa(a, b);
+	}
+}
