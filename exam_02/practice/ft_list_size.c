ff81f9396285ab01fbe1bf8b948090287abcfa25
Diogo-Serra <di-code@outlook.com>
2026-01-07 15:08:07 +0000

    updates

--- exam_02/practice/ft_list_size.c
+++ exam_02/practice/ft_list_size.c
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/07 00:10:30 by diosoare          #+#    #+#             */
-/*   Updated: 2026/01/07 14:49:34 by diosoare         ###   ########.fr       */
+/*   Updated: 2026/01/07 15:04:55 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 typedef struct s_person
 {
-	char			*name;
+	char			name[50];
 	int				age;
 	struct s_person	*next;
 }	t_person;
@@ -37,14 +37,25 @@ t_person	*add_person(char *name, int age)
 		new_person->name[i] = name[i];
 		i++;
 	}
+	new_person->name[i] = '\0';
 	new_person->age = age;
 	new_person->next = NULL;
 	return (new_person);
 }
 
-int		lst_size(t_person **head)
+ssize_t	lst_size(t_person **head)
 {
-	
+	t_person	*current;
+	ssize_t		count;
+
+	current = *head;
+	count = 0;
+	while (current)
+	{
+		count++;
+		current = current->next;		
+	}
+	return (count);
 }
 
 void	add_front(t_person **head, t_person *new_node)
@@ -57,6 +68,19 @@ void	add_front(t_person **head, t_person *new_node)
 
 int	main(void)
 {
-	
+	t_person	*head;
+	t_person	*person1;
+	t_person	*person2;
+	ssize_t		sizelst;
+
+	head = NULL;
+	person1 = add_person("Diogo", 35);
+	person2 = add_person("Di", 30);
+	add_front(&head, person1);	
+	add_front(&head, person2);
+	printf("%s , %d\n", person1->name, person1->age);
+	printf("%s , %d\n", person2->name, person2->age);
+	sizelst = lst_size(&head);
+	printf("%zu\n", sizelst);	
 	return (0);
 }
