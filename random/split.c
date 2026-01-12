4ae6d38c46ca7dc29106c5ecc5d8ad862359796b
Diogo-Serra <di-code@outlook.com>
2026-01-12 19:12:48 +0000

    updates

--- random/split.c
+++ random/split.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
+    char    source[] = "Hello World!";
+    char    sep = ' ';
+
     printf("Hello World\n");
+    printf("%s - %c\n", source, sep);
     return (0);
 }
 
