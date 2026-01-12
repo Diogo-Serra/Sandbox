897ddb593d52cf42f93838226b421487edf62954
Diogo-Serra <di-code@outlook.com>
2026-01-12 22:52:12 +0000

    updates

--- random/split.c
+++ random/split.c
@@ -15,13 +15,15 @@ char    *ft_strdup(char *source, char sep)
     dup = malloc(i + 1);
     if (!dup)
         return (NULL);
-    dup[i] = '\0';
     j = 0;
-    while (j < i)
+    i = 0;
+    while (source[i] && source[i] != sep)
     {
-        dup[j] = source[j];
+        dup[j] = source[i];
         j++;
+        i++;
     }
+    dup[j] = '\0';
     return (dup);
 }
 
@@ -53,6 +55,43 @@ int count_words(char *source, char sep)
     return (count);
 }
 
+void    free_heap(char **source, int size)
+{
+    int i;
+
+    i = 0;
+    while (size != -1)
+    {
+        free(source[i]);
+        size--;
+    }
+    free(source);
+}
+
+char    **ft_split(char *source, char sep)
+{
+    char    **split;
+    int     words;
+    int     i;
+
+    words = count_words(source, sep);
+    split = (char **)malloc(words * sizeof(char *));
+    if (!split)
+        return (NULL);
+    i = 0;
+    while (i < words)
+    {
+        while (*source == sep)
+            source++;
+        split[i] = ft_strdup(source, sep);
+        if (split[i])
+            return (free_heap(split, words), NULL);
+        source += lenword(source, sep);
+        i++;
+    }
+    return (split);
+}
+
 int main(void)
 {
     char    source[] = "Hello World! test!";
@@ -60,12 +99,22 @@ int main(void)
     char    *dup;
     int     count;
     int     len_word;
+    char    **split;
+    int     i;
 
     printf("%s\n", source);
     dup = ft_strdup(source, sep);
     count = count_words(source, sep);
     len_word = lenword(source, sep); 
     printf("%s\n%d\n%d\n", dup, count, len_word);
+    split = ft_split(source, sep);
+    i = 0;
+    while (i < count)
+    {
+        printf("%s\n", split[i]);
+        i++;
+    }
+    free(split);
     free(dup);
     return (0);
 }
