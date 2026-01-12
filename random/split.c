5ceb97b62183e9408faa81728029238393a508eb
Diogo-Serra <di-code@outlook.com>
2026-01-12 19:34:46 +0000

    updates

--- random/split.c
+++ random/split.c
@@ -25,15 +25,36 @@ char    *ft_strdup(char *source, char sep)
     return (dup);
 }
 
+int count_words(char *source, char sep)
+{
+    int i;
+    int count;
+
+    if (!source || !sep)
+        return (0);
+    i = 0;
+    count = 0;
+    while (source[i])
+    {
+        if (source[i] != sep && (i == 0 || source[i - 1] == sep))
+            count++;
+        i++;
+    }
+    return (count);
+}
+
 int main(void)
 {
-    char    source[] = "Hello World!";
+    char    source[] = "Hello World! test!";
     char    sep = ' ';
     char    *dup;
+    int     count;
 
-    printf("Hello World\n");
+    printf("%s\n", source);
     dup = ft_strdup(source, sep);
-    printf("%s\n", dup);
+    count = count_words(source, sep);
+    printf("%s\n%d\n", dup, count);
+    free(dup);
     return (0);
 }
 
