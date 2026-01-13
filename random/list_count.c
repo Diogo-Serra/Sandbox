5d21a8ce7de0b90fd834b220a7071edf8240b803
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-01-13 09:08:27 +0000

    updates

--- random/list_count.c
+++ random/list_count.c
@@ -11,6 +11,7 @@ t_person	*add_person(void *data)
 {
 	t_person 	*new_person;
 	char		*name;
+	char		*new_data;
 	int		i;
 
 	if (!data)
@@ -19,18 +20,28 @@ t_person	*add_person(void *data)
 	name = (char *)data;
 	i = 0;
 	while (name[i])
+		i++;
+	new_data = malloc((i + 1) * sizeof(char));
+	if (!new_data)
+		return (NULL);
+	i = 0;
+	while (name[i])
 	{
-		new_person->data[i] = name[i];
+		new_data[i] = name[i];
 		i++;
 	}
-	new_person[i] = '\0';
+	new_data[i] = '\0';
+	new_person->data = new_data;
 	new_person->next = NULL;
 	return (new_person);
 }
+
 int	main(void)
 {
 	t_person	*person1;
 	person1 = add_person("Diogo");
-	printf("%s\n", t_person->data);
+	printf("%s\n", (char *)person1->data);
+	free(person1->data);
+	free(person1);
 	return (0);
 }
