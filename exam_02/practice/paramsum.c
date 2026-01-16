a9c453a74591990b56725ce150fc5d3a8ac720b6
Diogo-Serra <di-code@outlook.com>
2026-01-16 14:44:21 +0000

    updates

--- exam_02/practice/paramsum.c
+++ exam_02/practice/paramsum.c
@@ -3,38 +3,38 @@
 
 int ft_atoi(char *source)
 {
-    int     i;
-    int     sign;
-    int     digit;
+	int i;
+	int sign;
+	int digit;
 
-    i = 0;
-    sign = 1;
-    while (source[i] == ' ' || (source[i] > 9 && source[i] < 13))
-        i++;
-    if (source[i] == '+' || source[i] == '-')
-    {
-        if (source[i] == '-')
-            sign *= -1;
-        i++;
-    }
-    digit = 0;
-    while (source[i] >= '0' && source[i] <= '9')
-        digit = digit * 10 + (source[i++] - '0');
-    return (digit * sign);
+	i = 0;
+	sign = 1;
+	while (source[i] == ' ' || (source[i] > 9 && source[i] < 13))
+		i++;
+	if (source[i] == '+' || source[i] == '-')
+	{
+		if (source[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	digit = 0;
+	while (source[i] >= '0' && source[i] <= '9')
+		digit = digit * 10 + (source[i++] - '0');
+	return (digit * sign);
 }
 
 int main(int argc, char **argv)
 {
-    int count;
-    int i;
+	int count;
+	int i;
 
-    if (argc <= 1)
-        return (printf("No Parameters\n"), 0);
-    count = 0;
-    i = 1;
-    while (i < argc)
-        count += ft_atoi(argv[i++]);
-    printf("Arg Sum: %d\n", count);
+	if (argc <= 1)
+		return (printf("No Parameters\n"), 0);
+	count = 0;
+	i = 1;
+	while (i < argc)
+		count += ft_atoi(argv[i++]);
+	printf("Arg Sum: %d\n", count);
 	printf("Arg number: %d", argc - 1);
-    return (0);
+	return (0);
 }
