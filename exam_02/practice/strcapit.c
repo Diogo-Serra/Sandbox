d299bae0ccfa25532ee207e0aba8cabe2220034e
Diogo-Serra <di-code@outlook.com>
2026-01-16 17:11:36 +0000

    updates

--- exam_02/practice/strcapit.c
+++ exam_02/practice/strcapit.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
     int     i;
 
     if (argc <= 1)
-        return (printf("Erro"), 0);
+        return (printf("Error"), 0);
     i = 1;
     while (i < argc)
     {
