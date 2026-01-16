255bd4f9f254cefdcf500269cfd4bcf314a473d9
Diogo-Serra <di-code@outlook.com>
2026-01-16 16:45:36 +0000

    updates

--- exam_02/practice/strcapit.c
+++ exam_02/practice/strcapit.c
@@ -1,14 +1,49 @@
 #include <stdio.h>
 
-int main(int argc, char **argv)
+void strcap(char *source)
 {
     int i;
+    int word;
+
+    i = 0;
+    word = 0;
+    while (source[i])
+    {
+        while ((source[i] == ' ') || (source[i] >= 9 && source[i] <= 13))
+            i++;
+        if (source[i] >= 'a' && source[i] <= 'z')
+        {
+            if (word == 0)
+            {
+                word = 1;
+                source[i++] += 32;
+            }
+            else if (word == 1)
+            {
+                if (source[i - 1] == ' ' || (source[i - 1] >= 9 && source[i - 1] <= 13)) 
+                    word = 0;
+                i++;
+            }
+        }
+        else {
+            i++;
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    int     i;
+    char    *cap;
 
     if (argc <= 1)
         return (printf("Erro"), 0);
     i = 1;
     while (i < argc)
-        printf("%s\n", argv[i++]);
+    {
+        cap = argv[i++];
+        printf("%s\n", cap);
+    }
     return (0);
 }
 
