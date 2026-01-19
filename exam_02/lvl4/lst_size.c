c5c249c260e249c56b6528fe85b3ae0e840a7d1b
Diogo-Serra <di-code@outlook.com>
2026-01-19 11:37:26 +0000

    updates

---
+++ exam_02/lvl4/lst_size.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lst_size.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/19 11:35:38 by diosoare          #+#    #+#             */
+/*   Updated: 2026/01/19 11:37:00 by diosoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+typedef struct s_person {
+	void			*data;
+	struct s_person	*next;
+}	t_person;
+
+int	main(int argc, char **argv)
+{
+
+	return (0);
+}
