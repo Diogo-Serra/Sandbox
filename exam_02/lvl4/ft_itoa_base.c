e1c81d9c0fbb60b1df89e0004adcfa7cf5bc9cc1
Diogo-Serra <di-code@outlook.com>
2026-01-19 10:15:09 +0000

    updates

--- exam_02/lvl4/ft_itoa_base.c
+++ exam_02/lvl4/ft_itoa_base.c
@@ -6,13 +6,43 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 09:25:35 by diosoare          #+#    #+#             */
-/*   Updated: 2026/01/19 09:58:01 by diosoare         ###   ########.fr       */
+/*   Updated: 2026/01/19 10:14:48 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+char	*ft_itoa(int n)
+{
+	char	arr[11];
+	ssize_t	nb;
+	int		i;
+	int		j;
+	char	*out;
+
+	i = 10;
+	nb = (ssize_t)n;
+	if (n == 0)
+		arr[--i] = '0';
+	if (n < 0)
+		nb *= -1;
+	while (nb)
+	{
+		arr[--i] = (nb % 10) + '0';
+		nb /= 10;
+	}
+	if (n < 0)
+		arr[--i] = '-';
+	out = malloc((11 - i) * sizeof(char));
+	if (!out)
+		return (NULL);
+	j = 0;
+	while (arr[i])
+		out[j++] = arr[i++];
+	return (out);
+}
 
 int	ft_atoi(char *source)
 {
@@ -38,16 +68,20 @@ int	ft_atoi(char *source)
 }
 int	main(int argc, char **argv)
 {
-	int	i;
-	int	digit;
+	int		i;
+	int		atoi;
+	char	*itoa;
 
 	if (argc <= 1)
 		return (printf("\n"));
 	i = 1;
 	while (i < argc)
 	{
-		digit = ft_atoi(argv[i++]);
-		printf("%d\n", digit);
+		atoi = ft_atoi(argv[i]);
+		printf("%d\n", atoi);
+		itoa = ft_itoa(atoi);
+		printf("%s\n", itoa);
+		i++;
 	}
 	return (0);
 }
