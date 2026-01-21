4db7867e6a3a8a253740c30f2186cf518561f9a5
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-01-21 13:59:02 +0000

    updates

--- exam_02/lvl4/lst_count.c
+++ exam_02/lvl4/lst_count.c
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 12:35:48 by diosoare          #+#    #+#             */
-/*   Updated: 2026/01/21 13:12:20 by diosoare         ###   ########.fr       */
+/*   Updated: 2026/01/21 13:58:20 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,36 @@ typedef struct s_person {
 t_person	*add_person(char *name)
 {
 	t_person	*new_person;
-	
+
 	if (!name)
 		return (NULL);
 	new_person = malloc(sizeof(t_person));
-	if (new_person)
+	if (!new_person)
 		return (NULL);
 	new_person->name = name;
 	new_person->next = NULL;
+	return (new_person);
+}
+
+void	add_back(t_person **head, t_person *node)
+{
+	t_person *current;
+
+	current = NULL;
+	if (!*head)
+	{
+		*head = node;
+		return ;		
+	}
+	current = *head;
+	while (current->next)
+		current = current->next;
+	current->next = node;
 }
 
 void	add_front(t_person **head, t_person *node)
 {
-	node->next = head;
+	node->next = *head;
 	*head = node;
 }
 
@@ -52,8 +69,36 @@ int	count_list(t_person *head)
 	return (count);
 }
 
-int	main(int argc, char **argv)
+void	print_list(t_person *head)
 {
+	t_person	*current;
 
+	current = head;
+	while (current)
+	{
+		if (!current->next)
+			printf("%s\n", current->name);
+		else
+			printf("%s -> ", current->name);			
+		current = current->next;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int			i;
+	t_person	*person;
+	t_person	*head;
+
+	// printf("here");
+	head = NULL;
+	i = 1;
+	while (i < ac)
+	{
+		person = add_person(av[i++]);
+		add_back(&head, person);
+	}
+	printf("%d\n", count_list(head));
+	print_list(head);
 	return (0);
 }
