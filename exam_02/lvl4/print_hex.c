9954fc7a9f63e15a196acc71b55a75a9965c22b0
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-01-21 11:42:10 +0000

    updates

---
+++ exam_02/lvl4/print_hex.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_hex.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/21 11:26:19 by diosoare          #+#    #+#             */
+/*   Updated: 2026/01/21 11:41:57 by diosoare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+int	ft_atoi(char *s)
+{
+	int	sign;
+	int	result;
+
+	sign = 1;
+	result = 0;
+	while (*s == ' ' || *s >= 9 && *s <= 13)
+		s++;
+	while (*s == '+' || *s == '-')
+		if (*s == '-')
+			sign *= -1;
+		s++;
+	while (*s >= '0' && *s <= '9')
+		result = result * 10 + (*s++ - '0');
+	return (result * sign);	
+}
+
+void	print_hex(int n)
+{
+	char	hex[] = "0123456789abcdef";
+	
+	if (n >= 16)
+		print_hex(n / 16);
+	write(1, &hex[n % 16], 1);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		print_hex(ft_atoi(argv[1]));
+	write(1, "\n", 1);
+	return (0);
+}
