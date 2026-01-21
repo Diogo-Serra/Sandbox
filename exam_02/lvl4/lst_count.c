ef96935a29b4faf26510f0766950c498835f9e03
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-01-21 13:12:33 +0000

    updates

--- exam_02/lvl4/lst_count.c
+++ exam_02/lvl4/lst_count.c
@@ -6,7 +6,7 @@
 /*   By: diosoare <diosoare@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 12:35:48 by diosoare          #+#    #+#             */
-/*   Updated: 2026/01/21 12:39:12 by diosoare         ###   ########.fr       */
+/*   Updated: 2026/01/21 13:12:20 by diosoare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,29 @@ t_person	*add_person(char *name)
 	new_person = malloc(sizeof(t_person));
 	if (new_person)
 		return (NULL);
-	
+	new_person->name = name;
+	new_person->next = NULL;
+}
+
+void	add_front(t_person **head, t_person *node)
+{
+	node->next = head;
+	*head = node;
+}
+
+int	count_list(t_person *head)
+{
+	t_person	*current;
+	int			count;
+
+	count = 0;
+	current = head;
+	while (current)
+	{
+		count++;
+		current = current->next;
+	}
+	return (count);
 }
 
 int	main(int argc, char **argv)
