25d3f5b51ffccbe91f30b7aed27f387a8ac27b2e
Diogo-Serra <di-code@outlook.com>
2026-01-22 17:07:29 +0000

    updates

---
+++ exam_02/practice/list_ops/list_algo.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::       ::::::::   */
+/*   list_algorithms.c                                  :+:      :+:     :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diosoare <diosoare@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/22 by diosoare              #+#    #+#             */
+/*   Updated: 2026/01/22 by diosoare             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "list_op. h"
+
+/*
+** Comparison function for sorting
+** Returns 0 if a > b (needs swap), 1 otherwise
+*/
+int	cmp(int a, int b)
+{
+	if (b < a)
+		return (0);
+	return (1);
+}
+
+/*
+** Equality function for duplicate detection
+** Returns 0 if equal, 1 if different
+*/
+int	equal(int a, int b)
+{
+	if (a == b)
+		return (0);
+	return (1);
+}
+
+/*
+** Sorts the list using bubble sort algorithm
+** Takes a comparison function pointer as parameter
+** Returns:  pointer to head of sorted list
+*/
+t_list	*sort_list(t_list *list, int (*cmp)(int, int))
+{
+	t_list	*head;
+	int		tmp;
+
+	head = list;
+	while (list->next)
+	{
+		if (cmp(list->data, list->next->data) == 0)
+		{
+			tmp = list->next->data;
+			list->next->data = list->data;
+			list->data = tmp;
+			list = head;
+		}
+		else
+			list = list->next;
+	}
+	return (head);
+}
+
+/*
+** Removes duplicate consecutive values from the list
+** Takes an equality function pointer as parameter
+** Returns: pointer to head of cleaned list
+*/
+t_list	*clean_dups(t_list *list, int (*equal)(int, int))
+{
+	t_list	*head;
+	t_list	*tmp;
+
+	head = list;
+	while (list->next)
+	{
+		if (equal(list->data, list->next->data) == 0)
+		{
+			tmp = list->next->next;
+			free(list->next);
+			list->next = tmp;
+			if (! tmp)
+				break ;
+		}
+		else
+			list = list->next;
+	}
+	return (head);
+}
---
+++ exam_02/practice/list_ops/list_core.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::       ::::::::   */
+/*   list_core.c                                        :+:      :+:     :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diosoare <diosoare@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/22 by diosoare              #+#    #+#             */
+/*   Updated: 2026/01/22 by diosoare             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "list_op. h"
+
+/*
+** Creates a new node with the given data
+** Returns:  pointer to new node, or NULL if malloc fails
+*/
+t_list	*add_node(int data)
+{
+	t_list	*new_node;
+
+	new_node = malloc(sizeof(t_list));
+	if (! new_node)
+		return (NULL);
+	new_node->data = data;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/*
+** Adds a node to the front of the list
+** This modifies the head pointer
+*/
+void	add_front(t_list **head, t_list *node)
+{
+	node->next = *head;
+	*head = node;
+}
+
+/*
+** Prints all elements in the list with arrows between them
+*/
+void	print_list(t_list *head)
+{
+	t_list	*current;
+
+	current = head;
+	if (!current)
+	{
+		printf("(empty list)\n");
+		return ;
+	}
+	while (current)
+	{
+		printf("%d", current->data);
+		if (current->next)
+			printf(" -> ");
+		current = current->next;
+	}
+	printf("\n");
+}
+
+/*
+** Frees all nodes in the list
+** Important: Always call this before program ends! 
+*/
+void	free_list(t_list *head)
+{
+	t_list	*current;
+	t_list	*tmp;
+
+	current = head;
+	while (current)
+	{
+		tmp = current->next;
+		free(current);
+		current = tmp;
+	}
+}
+
+/*
+** Simple atoi implementation
+** Handles spaces, signs, and digits
+*/
+int	ft_atoi(char *s)
+{
+	int	sign;
+	int	result;
+
+	sign = 1;
+	result = 0;
+	while (*s == ' ' || (*s >= 9 && *s <= 13))
+		s++;
+	if (*s == '-' || *s == '+')
+	{
+		if (*s == '-')
+			sign *= -1;
+		s++;
+	}
+	while (*s >= '0' && *s <= '9')
+		result = result * 10 + (*s++ - '0');
+	return (result * sign);
+}
