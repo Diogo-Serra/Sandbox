209257c6549caffce43d85adad8156931153a6e8
Diogo-Serra <di-code@outlook.com>
2026-03-19 17:54:40 +0000

    updates

--- python/exercises/classes/ex7.py
+++ python/exercises/classes/ex7.py
@@ -5,26 +5,27 @@ class Calc:
     def __init__(self):
         pass
 
-    def add(*numbers):
+    def add(self, *numbers):
         total = 0
         for n in numbers:
             total += n
         return total
 
-    def multiply(*numbers):
+    def multiply(self, *numbers):
         total = 1
         for n in numbers:
             total *= n
         return total
 
-    def power(base, exponent=2):
+    def power(self, base, exponent=2):
         return (base ** exponent)
 
 
 def tester():
-    print(Calc.add(1, 2, 3))
-    print(Calc.multiply(2, 4))
-    print(Calc.power(2))
+    calc = Calc()
+    print(calc.add(1, 2, 3))
+    print(calc.multiply(2, 4))
+    print(calc.power(2))
 
 
 tester()
