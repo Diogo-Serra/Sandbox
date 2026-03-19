726e49d1b477ec3b1fdb443728f7043d7486986f
Diogo-Serra <di-code@outlook.com>
2026-03-19 11:18:51 +0000

    updates

---
+++ python/exercises/classes/ex3.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+class Person:
+    def __init__(self,):
+        self.__age = None
+
+    @property
+    def p(self):
+        return (f"Age: {self.__age}")
+
+    def getp(self):
+        return self.__age
+
+    def setp(self, new_age):
+        try:
+            age_int = int(new_age)
+            if age_int <= 0 or age_int >= 150:
+                print("Not a valid age")
+                return
+            self.__age = age_int
+        except ValueError:
+            print("Not a valid age!")
+            return
+
+
+def tester():
+    person1 = Person()
+    person1.setp(0)
+
+
+tester()
