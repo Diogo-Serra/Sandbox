37254020a7dc837c2b12e117c9db95f1dc94653c
Diogo-Serra <di-code@outlook.com>
2026-03-20 15:58:38 +0000

    updates

--- python/exercises/classes/ex11.py
+++ python/exercises/classes/ex11.py
@@ -14,7 +14,7 @@ class Temperature:
 
     @staticmethod
     def celsius_to_fahrenheit(celsius):
-        return (celsius * 9/5)
+        return (celsius * 9/5) + 32
 
     @staticmethod
     def fahrenheit_to_celsius(fahrenheit):
