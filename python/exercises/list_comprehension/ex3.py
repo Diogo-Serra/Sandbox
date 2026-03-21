13b9d163c8d694fcf261e3741886ceecdd2bb55d
Diogo-Serra <di-code@outlook.com>
2026-03-21 20:55:28 +0000

    updates

---
+++ python/exercises/list_comprehension/ex3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+# Ex 3: Return the lengths of each word
+# Input:  ["hi", "hello", "hey"]
+# Output: [2, 5, 3]
+def word_lengths(words):
+    pass  # TODO
+"""
+
+
+def ex3():
+    list1 = ["hi", "hello", "hey"]
+    list2 = [len(x) for x in list1]
+    print(list2)
+
+
+ex3()
