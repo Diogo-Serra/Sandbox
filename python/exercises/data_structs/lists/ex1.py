e4c90c928b3f5e4cb6fff76e9be199e7dd7572dc
Diogo Alexandre Soares Serra <di-code@outlook.com>
2026-03-27 15:58:21 +0000

    updates

---
+++ python/exercises/data_structs/lists/ex1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+easy
+Shopping cart
+Create a list of 5 grocery items. Add 2 more items, remove one
+by name, and print the final list sorted alphabetically.
+"""
+
+
+def ex1() -> None:
+    grocery_list = [
+        "Fruit1",
+        "Fruit2",
+        "Fruit3",
+        "Fruit4",
+        "Fruit5",]
+
+    grocery_list.append("Banana")
+    grocery_list.append("Tomato")
+    print(grocery_list)
+
+
+ex1()
