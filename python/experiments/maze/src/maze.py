b5bab4bfa82fc614846eae64361d45ce72dc2a6b
Diogo-Serra <di-code@outlook.com>
2026-04-22 02:32:55 +0100

    updates

--- python/experiments/maze/src/maze.py
+++ python/experiments/maze/src/maze.py
@@ -22,10 +22,16 @@ class Maze(BaseModel):
         exit_x, exit_y = self.EXIT
         if entry_x or exit_x > self.WIDTH:
             raise ValueError('Coordinates cannot exceed Width')
-        if entry_x or exit_x < self.WIDTH:
-            raise ValueError('Coordinates cannot be lower then Width')
+        if entry_x or exit_x < 0:
+            raise ValueError('Coordinates cannot be negative')
         if entry_y or exit_y > self.HEIGHT:
             raise ValueError('Coordinates cannot exceed Height')
-        if entry_y or exit_y < self.HEIGHT:
-            raise ValueError('Coordinates cannot be lower then Height')
+        if entry_y or exit_y < 0:
+            raise ValueError('Coordinates cannot be negative')
         return self
+
+    def __str__(self) -> str:
+        return (f"Width: {self.WIDTH}\nHeight: {self.HEIGHT}\n"
+                f"Entry: {self.ENTRY}\nExit: {self.EXIT}\n"
+                f"Output_file: {self.OUTPUT_FILE}\n"
+                f"Perfect: {self.PERFECT}")
