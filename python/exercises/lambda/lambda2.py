5b2681eb311c654e453490ac9aebcc86ca0915e2
Diogo-Serra <di-code@outlook.com>
2026-04-24 23:39:37 +0100

    updates

--- python/exercises/lambda/lambda2.py
+++ python/exercises/lambda/lambda2.py
@@ -12,13 +12,22 @@ def mage_counter() -> Callable:
 
 
 def spell_accumulator(initial_power: int) -> Callable:
-    spell_power: int = 0
+    spell_power: int = initial_power
 
     def accumulator():
         nonlocal spell_power
         spell_power += 10
         return spell_power
     return accumulator
+
+
+def enchantment_factory(enchantment_type: str) -> Callable:
+    enchantment_type: str = enchantment_type + " "
+
+    def enchant(item: str):
+        nonlocal enchantment_type
+        return enchantment_type + item
+    return enchant
 
 
 if __name__ == "__main__":
@@ -28,7 +37,10 @@ if __name__ == "__main__":
     print(counter())
     print(counter())
 
-    accumulator = spell_accumulator(10)
+    accumulator = spell_accumulator(20)
     print(accumulator())
     print(accumulator())
     print(accumulator())
+
+    enchantment_factory = enchantment_factory("Flamming")
+    print(enchantment_factory("Sword"))
