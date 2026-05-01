49614b1d67906d2a187fe421bc8a9c9cf5b0d0ed
Diogo-Serra <di-code@outlook.com>
2026-05-01 02:44:48 +0100

    final

--- leetcode/python/remove_duplicates.py
+++ leetcode/python/remove_duplicates.py
@@ -1,8 +1,21 @@
+from typing import List
+
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        pass
+        k = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[k] = nums[i]
+                k += 1
+        return k
 
 
 if __name__ == "__main__":
-    pass
+
+    nums_list = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+
+    solution = Solution()
+    r = solution.removeDuplicates(nums_list)
+    print(r)
