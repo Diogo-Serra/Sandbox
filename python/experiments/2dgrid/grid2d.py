a5f4556f865b59d8e05d845d39eff4d098c5707b
Diogo-Serra <di-code@outlook.com>
2026-05-20 17:14:02 +0100

    updates

--- python/experiments/2dgrid/grid2d.py
+++ python/experiments/2dgrid/grid2d.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
 from sys import argv, exit
 
-coordinates = {
-    'NORTH': 0x1,
-    'EAST': 0x2,
-    'SOUTH': 0x4,
-    'WEST': 0x8}
 
-if len(argv) == 1:
-    user = input("Insert Width and Height: \n").strip().split()
-    if len(user) == 2:
-        width = int(user[0])
-        height = int(user[1])
-        grid = [[0xF]*width for _ in range(height)]
-        for row in grid:
-            for cell in row:
-                print(hex(cell)[2].capitalize(), end='.')
-            print(end='\n')
-        print()
-        for row in grid:
-            for cell in row:
-                print(bin(cell)[2:], end='.')
-            print('\n')
-    else:
-        print("Exiting")
-        exit(1)
+def grid_creation(width: int, height: int):
+
+    grid = [[0xF]*width for _ in range(height)]
+    for row in grid:
+        for cell in row:
+            print(hex(cell)[2].capitalize(), end='.')
+        print(end='\n')
+    print()
+    for row in grid:
+        for cell in row:
+            print(bin(cell)[2:], end='.')
+        print('\n')
+
+
+if __name__ == '__main__':
+
+    if len(argv) == 1:
+        user = input("Insert Width and Height: ").strip().split()
+        if len(user) == 2:
+            width = int(user[0])
+            height = int(user[1])
+            try:
+                grid_creation(width, height)
+            except Exception as error:
+                print(error)
+        else:
+            print("Exiting")
+            exit(1)
