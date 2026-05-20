7dec644f4f08839307067d06a7307477280dd4c8
Diogo-Serra <di-code@outlook.com>
2026-05-20 16:55:50 +0100

    updates

--- python/experiments/2dgrid/grid2d.py
+++ python/experiments/2dgrid/grid2d.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 from sys import argv, exit
 
+coordinates = {
+    'NORTH': 0x1,
+    'EAST': 0x2,
+    'SOUTH': 0x4,
+    'WEST': 0x8}
 
 if len(argv) == 1:
     user = input("Insert Width and Height: \n").strip().split()
@@ -10,8 +15,13 @@ if len(argv) == 1:
         grid = [[0xF]*width for _ in range(height)]
         for row in grid:
             for cell in row:
-                print(hex(cell)[2].capitalize(), end='')
+                print(hex(cell)[2].capitalize(), end='.')
             print(end='\n')
+        print()
+        for row in grid:
+            for cell in row:
+                print(bin(cell)[2:], end='.')
+            print('\n')
     else:
         print("Exiting")
         exit(1)
