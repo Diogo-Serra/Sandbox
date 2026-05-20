0ac80c686bd2fb7663e579e389c0519cd2c2e30d
Diogo-Serra <di-code@outlook.com>
2026-05-20 16:22:17 +0100

    updates

--- python/experiments/2dgrid/grid2d.py
+++ python/experiments/2dgrid/grid2d.py
@@ -5,8 +5,13 @@ from sys import argv, exit
 if len(argv) == 1:
     user = input("Insert Width and Height: \n").strip().split()
     if len(user) == 2:
-        width = user[0]
-        height = user[1]
+        width = int(user[0])
+        height = int(user[1])
+        grid = [[0xF]*width for _ in range(height)]
+        for row in grid:
+            for cell in row:
+                print(int(cell), end='')
+            print(end='\n')
     else:
         print("Exiting")
         exit(1)
