49a8867e6ecc1ba9aab684b0d3d3c9806fa25e85
Diogo-Serra <di-code@outlook.com>
2026-05-21 20:41:41 +0100

    updates

--- python/experiments/2dgrid/grid2d.py
+++ python/experiments/2dgrid/grid2d.py
@@ -6,30 +6,36 @@ def grid_creation(width: int, height: int):
 
     print()
     grid = [[0xF]*width for _ in range(height)]
-    for row in grid:
-        for cell in row:
-            print(hex(cell)[2].capitalize(), end='.')
-        print(end='\n')
+    for y, row in enumerate(grid):
+        for x, col in enumerate(row):
+            print(grid[x][y], end='')
+        print('\n', end='')
     print()
-    for row in grid:
-        for cell in row:
-            print(bin(cell)[2:], end='.')
-        print('\n')
 
-    print(grid[0][0])
+    for y, row in enumerate(grid):
+        for x, col in enumerate(row):
+            print(f"({x}, {y})", end='')
+        print('\n', end='')
+    print()
 
 
 if __name__ == '__main__':
 
     if len(argv) == 1:
-        user = input("Insert Width and Height: ").strip().split()
-        if len(user) == 2:
-            width = int(user[0])
-            height = int(user[1])
-            try:
+
+        try:
+
+            user = input("Insert Width and Height: ").strip().split()
+
+            if len(user) == 2:
+                width = int(user[0])
+                height = int(user[1])
                 grid_creation(width, height)
-            except Exception as error:
-                print(error)
-        else:
-            print("Exiting")
+            else:
+                print("Exiting\n"
+                      "Usage:<int> <int>")
+                exit(1)
+
+        except (Exception, BaseException) as error:
+            print(error)
             exit(1)
