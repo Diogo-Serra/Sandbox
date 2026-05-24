ba77da974cf60f99dcc983e93b2506fcb96b48db
Diogo-Serra <di-code@outlook.com>
2026-05-24 01:39:14 +0100

    practice updates

--- python/experiments/dfs/src/dfs.py
+++ python/experiments/dfs/src/dfs.py
@@ -1,26 +1,14 @@
-# Maze generator - DFS: Depth-field search
-from random import choice
+# Maze generator - DFS
 
+def dfs_generator(grid: list[list[int]]) -> list[list[int]]:
 
-def dfs_generator() -> None:
-
-    # (bit, dx, dy, bit-mirror)
+    # (bit, dx, dy, mirror)
     DIRECTIONS = [
-        (0, 0, -1, 2),  # North
-        (1, +1, 0, 3),  # East
-        (2, 0, +1, 0),  # South
-        (3, -1, 0, 1),  # West
+        (0, 0, -1, 2),  # NORTH
+        (1, +1, 0, 3),  # EAST
+        (2, 0, +1, 0),  # SOUTH
+        (3, -1, 0, 1),  # WEST
     ]
-
-    width, height = 20, 10
-    grid = [[0xF] * height for _ in range(width)]
-
-    print()
-    for y in range(height):
-        for x in range(width):
-            print(format(grid[x][y], 'X'), end='')
-        print()
-    print()
 
     width = len(grid)
     height = len(grid[0])
@@ -37,23 +25,4 @@ def dfs_generator() -> None:
             if (0 <= nx < width
                     and 0 <= ny < height
                     and (nx, ny) not in visited):
-                neighbors.append((nx, ny, bit, mirror))
-
-        if neighbors:
-            nx, ny, bit, mirror = choice(neighbors)
-            grid[x][y] &= ~(1 << bit)
-            grid[nx][ny] &= ~(1 << mirror)
-            visited.add((nx, ny))
-            stack.append((nx, ny))
-        else:
-            stack.pop()
-
-    print()
-    for y in range(height):
-        for x in range(width):
-            print(format(grid[x][y], 'X'), end='')
-        print()
-    print()
-
-
-dfs_generator()
+                neighbors.append(nx, ny)
