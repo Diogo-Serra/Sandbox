31df72ee75d7aef763342c681d99f20bac66d832
Diogo-Serra <di-code@outlook.com>
2026-05-25 18:23:41 +0100

    updates

--- python/experiments/dfs/src/dfs_example.py
+++ python/experiments/dfs/src/dfs_example.py
@@ -1,14 +1,14 @@
-# DFS Maze generator
-from random import choice
-
+# Maze generator - DFS
 
 def dfs_generator(grid: list[list[int]]) -> list[list[int]]:
+    from random import choice
 
+    # (bit, dx, dy, mirror)
     DIRECTIONS = [
-        (0, 0, -1, 2),  # North
-        (1, +1, 0, 3),  # East
-        (2, 0, +1, 0),  # South
-        (3, -1, 0, 1)   # West
+        (0, 0, -1, 2),  # NORTH
+        (1, +1, 0, 3),  # EAST
+        (2, 0, +1, 0),  # SOUTH
+        (3, -1, 0, 1),  # WEST
     ]
 
     width = len(grid)
@@ -17,15 +17,16 @@ def dfs_generator(grid: list[list[int]]) -> list[list[int]]:
     stack: list[tuple[int, int]] = [(0, 0)]
 
     while stack:
+
         x, y = stack[-1]
 
         neighbors = []
         for bit, dx, dy, mirror in DIRECTIONS:
             nx, ny = x + dx, y + dy
-            if ((0 <= nx < width)
-                    and (0 <= ny < height)
+            if (0 <= nx < width
+                    and 0 <= ny < height
                     and (nx, ny) not in visited):
-                neighbors.append((nx, ny, bit, mirror))
+                neighbors.append(nx, ny, bit, mirror)
 
         if neighbors:
             nx, ny, bit, mirror = choice(neighbors)
